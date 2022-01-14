1875eee07a3149fa048a44d7f77a0c9d7828e573
Matías Méndez <58478529+matiasandov@users.noreply.github.com>
2022-01-14 13:46:12 -0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,15 +8,15 @@ void main() {
 
 
 class MyApp extends StatelessWidget {
-   MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   //este es un widget y todo lo de adentro también son widgets
   Widget build(BuildContext context) {
 
     return  MaterialApp(
-        title: 'Startup Name Generator',
-        home: RandomWords(),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
@@ -35,6 +35,8 @@ class _RandomWordsState extends State<RandomWords> {
   //vas a tener una lista de WordPairs y el estilo de la lista que podras acceder desde donde quieras
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  //este es un set global para guardar las favoritas
+  final _saved = <WordPair>{};
 
   Widget _buildSuggestions() {
 
@@ -62,11 +64,76 @@ class _RandomWordsState extends State<RandomWords> {
 
   //crea las rows
   Widget _buildRow(WordPair pair) {
+    //bool para checar que el pair  de la row ya este gurdado en favoritos o no
+    final alreadySaved = _saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         //se llama al estilo global creado antes
         style: _biggerFont,
+      ),
+      //Icono de corazon
+      trailing: Icon(     // NEW from here...
+
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        //si alreeadySaved es true lo pone en rojo, sino vacio
+        color: alreadySaved ? Colors.red : null,
+        //supongo que es un mensaje de consola
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+
+      //metodo para anadir
+      onTap: () {      // NEW lines from here...
+        setState(() {
+          //se ya estaba guardada y lo vuelves a dar tap, la borrara
+          if (alreadySaved) {
+            _saved.remove(pair);
+            //si no estaba guardada, la guarda
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },               // ... to here.
+    );
+  }
+
+  //este metodo hara push (agregar) a la ruta a la que se quiere ir al stack de navegacion del obejto navigator
+  void _pushSaved() {
+    Navigator.of(context).push(
+      //widget de builder para abrir nueva pagina al hacer push al stack de navegacion
+      MaterialPageRoute<void>(
+        builder: (context) {
+
+          //creara lista de tiles con cada elemento de la lista de _saved, que guarda las sugerencias
+          //con map, para cada elemento define su texto y estilo
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+
+          //aqui se guardara la lsita creada arriba pero con divisiones, la cual sera cargda en el body
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+              : <Widget>[];
+
+          //estructura de la pagina
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
       ),
     );
   }
@@ -77,6 +144,18 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title:  Text('Startup Name Generator'),
+
+        //acciones de interacion con la AppBar
+        actions: [
+          // Icono para abrir lista de pestanas
+          IconButton(
+            icon: const Icon(Icons.list),
+            //al presionarse llamara a este metodo
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+
       ),
       //en el body ira el widget que crea la lsita
       body: _buildSuggestions(),
@@ -86,5 +165,5 @@ class _RandomWordsState extends State<RandomWords> {
 
 
 
-  //los metodos que quieras llamar en build siempre iran abajo de esta funcion y dentro de la clase
+//los metodos que quieras llamar en build siempre iran abajo de esta funcion y dentro de la clase
 }
